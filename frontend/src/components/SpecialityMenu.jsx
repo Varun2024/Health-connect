4b87c10862911f8a633adf55f9094aa0949ea9c8
Varun2024 <marvellous079@gmail.com>
2024-10-07 22:16:59 +0530

    FrontendAndAdminPanel

---
+++ frontend/src/components/SpecialityMenu.jsx
@@ -0,0 +1,22 @@
+
+import { specialityData } from '../assets/assets'
+import { Link } from 'react-router-dom'
+
+const SpecialityMenu = () => {
+    return (
+        <div id='speciality' className='flex flex-col items-center gap-4 py-16 mt-10 text-gray-900 mx-15  ' >
+            <h1 className='text-3xl font-medium'>Find by Speciality</h1>
+            <p className='sm:w-1/3 text-center text-sm'>Simply browse through our extensive list of trusted doctors, schedule your appointment hassle-free.</p>
+            <div className='flex sm:justify-center gap-4 pt-5 w-full overflow-scroll '>
+                {specialityData.map((item, index) => (
+                    <Link to={`/doctors/${item.speciality}`} onClick={() => scrollTo(0, 0)} className='flex flex-col justify-between items-center text-xs p-3 my-5 cursor-pointer flex-shrink-0 hover:translate-y-[-10px] transition-all duration-400 ease-in-out delay-150 hover:shadow-lg hover:border hover:shadow-orange-500 rounded-md' key={index}>
+                        <img className='w-16 sm:w-24 mb-2 ' src={item.image} alt="" />
+                        <p>{item.speciality}</p>
+                    </Link>
+                ))}
+            </div>
+        </div>
+    )
+}
+
+export default SpecialityMenu
